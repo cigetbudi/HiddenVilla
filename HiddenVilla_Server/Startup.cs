568a0bbb920ef514a69afae4bc1fccd04e7e8a4e
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-23 02:15:39 +0700

    Sec 4 - Add ApplicationDbContext

--- HiddenVilla_Server/Startup.cs
+++ HiddenVilla_Server/Startup.cs
@@ -24,7 +24,7 @@ namespace HiddenVilla_Server
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) 
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
