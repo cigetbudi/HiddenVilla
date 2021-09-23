9c7e7a7f8c440ddef5cdc631eb90200bda054f0f
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-23 16:09:42 +0700

    Sec 4 - Hotel Room Repository Interface

---
+++ Business/Repository/IRepository/IHotelRoomInterface.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    public interface IHotelRoomInterface
+    {
+        public Task<HotelRoomDTO> CreateHotelRoom(HotelRoomDTO hotelRoomDTO);
+        public Task<HotelRoomDTO> UpddateHotelRoom(int roomId, HotelRoomDTO hotelRoomDTO);
+        public Task<IEnumerable<HotelRoomDTO>> GetHotelRoom(int roomId);
+        public Task<HotelRoomDTO> IsSameNameRoomAlreadyPresent(string name);
+    }
+}
