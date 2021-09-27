dac8690de0e948eaf03f0fc1adef735feb59c8db
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-28 00:17:43 +0700

    Sec 6 - HotelImagesRepository.cs

---
+++ Business/Repository/HotelImagseRepository.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Business.Repository.IRepository;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository
+{
+    public class HotelImagseRepository : IHotelImagesRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        public HotelImagseRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _mapper = mapper;
+            _db = db;
+        }
+        public async Task<int> CreateHotelImage(HotelRoomImageDTO imageDTO)
+        {   //model parameter = _mapper.Map<ViewModel, Model>(parameter)
+            var image = _mapper.Map<HotelRoomImageDTO, HotelRoomImage>(imageDTO);
+            await _db.HotelRoomImages.AddAsync(image);
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteHotelImageById(int imageId)
+        {
+            var image = await _db.HotelRoomImages.FindAsync(imageId);
+            _db.HotelRoomImages.Remove(image);
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteHotelImageByRoomId(int roomId)
+        {
+            var imageList = await _db.HotelRoomImages.Where(x => x.RoomId == roomId).ToListAsync();
+            _db.HotelRoomImages.RemoveRange(imageList);
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<HotelRoomImageDTO>> GetHotelImages(int roomId)
+        {
+            //konvert model ke ViewModel
+                return _mapper.Map<IEnumerable<HotelRoomImage>, IEnumerable<HotelRoomImageDTO>>(
+                await _db.HotelRoomImages.Where(x => x.RoomId == roomId).ToListAsync());
+        }
+    }
+}
