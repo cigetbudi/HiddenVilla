80471bc3d87e90840b486c51b9f5e6b9b24b6e1f
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-27 22:12:33 +0700

    Sec 6 - HotelRoomImage Interface and Repository

---
+++ Business/Repository/IRepository/IHotelImagesRepository.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    public interface IHotelImagesRepository
+    {
+        //Create
+        public Task<int> CreateHotelImage(HotelRoomImageDTO image);
+
+        //Delete satuan per Id
+        public Task<int> DeleteHotelImageById(int imageId);
+
+        //Delete image per room
+        public Task<int> DeleteHotelImageByRoomId(int roomId);
+
+        //get all image by room Id
+        public Task<IEnumerable<HotelRoomImageDTO>> GetHotelImages(int roomId); 
+    }
+}
