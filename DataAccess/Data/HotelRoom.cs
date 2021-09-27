4a768df4e432a08e7d31f8cf4207bd2cf8aadcec
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-27 21:53:23 +0700

    Sec 6 - HotelRoomImage Model

--- DataAccess/Data/HotelRoom.cs
+++ DataAccess/Data/HotelRoom.cs
@@ -23,5 +23,7 @@ namespace DataAccess.Data
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string UpdatedBy { get; set; }
         public DateTime UpdatedDate { get; set; }
+        //virtual ga perlu migrasi
+        public virtual ICollection<HotelRoomImage> HotelRoomImages { get; set; }
     }
 }
